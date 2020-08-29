ee448679fe76db2a707a61e2221a357532612d32
DimonKeval <kevaldima@gmail.com>
2020-08-29 12:10:59 +0200

    Checking a Git activity

---
+++ View/src/main/java/AppLauncger.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AppLauncger {
+    public static void main(String[] args) {
+        Scanner scanFromConsole = new Scanner(System.in);
+        System.out.println(" - - - - - - - - - - - - \nStarting the program.\n - - - - - - - - - - - - \n");
+
+
+    }
+}
